c11d570033f808a38a5bab946adb58a8c6c1cca2
Sabrinahub1 <Sabrina.washington43@yahoo.com>
2020-08-18 19:14:25 -0400

    added question

--- TheGuessinggame.py
+++ TheGuessinggame.py
@@ -3,15 +3,16 @@ number = random.randint(1,10)
 # print number
 #Works in Python2
 
-player_name = input("Hello winner! What's your first name?")
+player_name = input("Hello Winner! What's your first name?")
+player_question = input ("Want to try my new game?")
 #Works in Python3
 
 number_of_guesses = 0
 
-print('What up' + player_name + 'I am thinking of a number between 1 and 10:')
+print('What up ' + player_name + ' Lets get started' + ' I am thinking of a number between 1 and 10:')
 #while loop
 while number_of_guesses < 5:
-    #pyhton's inbuilt method
+    #python's inbuilt method
     guess = int(input())
     #number of total guesses
     number_of_guesses += 1
